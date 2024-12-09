4b457a6d278c4aafa71838d6fbb7499f1554f53e
Md Intakhab <115621901+Intakhab08@users.noreply.github.com>
2024-12-09 19:33:54 +0000

    counter program

--- basicapp/src/App.js
+++ basicapp/src/App.js
@@ -1,7 +1,26 @@
+import { useState } from "react"
 
 function App() {
+  let [counter, setCounter] = useState(0);
+
+  const addValue = () => {
+    if(counter<20){
+    setCounter(counter+1);
+  }
+  }
+  const removeValue = () => {
+    if(counter>0){
+    setCounter(counter-1);
+  }
+  }
   return (
+    <>
     <h1>Welcome to Github Vs code console</h1>
+    <h2>{counter}</h2>
+    <button onClick={addValue}>Add Value</button>
+    <br/>
+    <button onClick={removeValue}>Decrease Value</button>
+    </>
   );
 }
 
